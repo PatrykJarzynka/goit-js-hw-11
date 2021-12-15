21514a2287183d9669617e992bb5fc59284e0ac1
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-12-15 21:51:40 +0100

    nie wiem

--- src/js/costam.js
+++ src/js/costam.js
@@ -1,9 +1,32 @@
-const axios = require('axios').default;
 
-axios({
-  method: 'get',
-  url: 'https://pixabay.com/api/?key=24835588-34c67f39a9342d1bd89adf1b2&q=yellow+flowers&image_type=photo'
-}).then(function (response) {
-    const { data } = response;
-    console.log(data);
+
+const searchButton = document.querySelector('button');
+// const gallery = document.querySelector(".gallery")
+
+// const searchForImages = () => {
+//   axios({
+//     method: 'get',
+//     url: 'https://pixabay.com/api/?key=24835588-34c67f39a9342d1bd89adf1b2&q=yellow+flowers&image_type=photo',
+//   }).then(response => {
+//       const { data } = response;
+//       console.log(data);
+//   });
+// };
+
+// const placeImages = () => {
+//     searchForImages
+// }
+
+// const createMarkup = (elements) => {
+//     const markup = elements.map(element => {
+//         return `<div>
+//                 <img src="${element.webformatURL}"/>
+//             </div>`;
+//     }).join("");
+//     gallery.innerHTML = markup;
+// }
+
+searchButton.addEventListener('click', (event) => {
+    event.preventDefault;
+    console.log("xd");
 });
