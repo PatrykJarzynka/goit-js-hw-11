8f9c0517f25af7e7387c27d20290076463f2e1ee
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-12-16 16:22:22 +0100

    nie działa nic

--- src/js/costam.js
+++ src/js/costam.js
@@ -1,32 +1,45 @@
 const axios = require('axios');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.search-form');
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector('.gallery');
+const input = document.querySelector('input[name="searchQuery"]');
 
-const searchForImages = () => {
-  return axios({
-    method: 'get',
-    url: 'https://pixabay.com/api/?key=24835588-34c67f39a9342d1bd89adf1b2&q=yellow+flowers&image_type=photo',
-  }).then(response => {
-      const { data } = response;
-      return data;
-  });
+const searchForImages = async name => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `https://pixabay.com/api/?key=24835588-34c67f39a9342d1bd89adf1b2&q=${name}&image_type=photo`,
+    });
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const placeImages = () => {
-    searchForImages().then(element => createMarkup(element.hits));
-}
+  const name = input.value;
+  searchForImages(name).then(element => createMarkup(element.hits));
+};
 
-const createMarkup = (elements) => {
-    const markup = elements.map(element => {
-        return `<div>
-                <img src="${element.webformatURL}"/>
+const createMarkup = elements => {
+  const markup = elements
+    .map(element => {
+      return `<div class="photo-card">
+                <a href="${element.largeImageURL}">
+                    <img src="${element.webformatURL}" alt="${element.tags}"/>
+                </a>
             </div>`;
-    }).join("");
-    gallery.innerHTML = markup;
-}
+    })
+    .join('');
+  gallery.innerHTML = markup;
+};
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    placeImages();
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  placeImages();
 });
+
+
