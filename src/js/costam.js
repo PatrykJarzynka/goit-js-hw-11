3059f939e44429e7c954efc2a5e19541b976d223
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-12-17 20:39:01 +0100

    Prace trwają

--- src/js/costam.js
+++ src/js/costam.js
@@ -5,24 +5,26 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const input = document.querySelector('input[name="searchQuery"]');
-const moreImages = document.querySelector(".load-more");
-let previousName = "";
+const moreImages = document.querySelector('.load-more');
+let previousName = ' ';
 let page = 1;
+let sumOfImages = 0;
 
 const searchForImages = async newName => {
   try {
-    console.log(page,newName,previousName);
     const params = new URLSearchParams({
+      page: page,
       per_page: 40,
       image_type: 'photo',
       orientation: 'horizontal',
-      safesearch: true
+      safesearch: true,
     });
     const response = await axios({
       method: 'get',
-      url: `https://pixabay.com/api/?key=24835588-34c67f39a9342d1bd89adf1b2&q=${newName}&${params}&page=${page}`,
+      url: `https://pixabay.com/api/?key=24835588-34c67f39a9342d1bd89adf1b2&q=${newName}&${params}`,
     });
     const { data } = response;
+    console.log(data)
     return data;
   } catch (error) {
     console.log(error.message);
@@ -32,11 +34,13 @@ const searchForImages = async newName => {
 const placeImages = () => {
   const name = input.value;
   searchForImages(name)
-    .then(element => createMarkup(element.hits,name))
+    .then(element => createMarkup(element.hits, name, element.totalHits))
     .then(() => new SimpleLightbox('.gallery a'));
 };
 
-const createMarkup = (elements,newName) => {
+const createMarkup = (elements, newName, allHits) => {
+  sumOfImages += elements.length;
+  if(sumOfImages >= allHits) console.log("")
   const markup = elements
     .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
       return `<div class="photo-card">
@@ -63,8 +67,7 @@ const createMarkup = (elements,newName) => {
     .join('');
   if (newName !== previousName) {
     gallery.innerHTML = markup;
-  }
-  else gallery.insertAdjacentHTML('beforeend', markup);
+  } else gallery.insertAdjacentHTML('beforeend', markup);
   previousName = newName;
 };
 
@@ -73,15 +76,13 @@ const loadMore = () => {
   placeImages();
 };
 
-
 form.addEventListener('submit', event => {
   event.preventDefault();
   if (input.value !== previousName) {
     page = 1;
     placeImages();
+    moreImages.classList.remove("not__visable");
   } else return;
 });
 
-moreImages.addEventListener("click", loadMore);
-
-
+moreImages.addEventListener('click', loadMore);
